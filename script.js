8de1a09b4b5ad8b33724f0f63a0c8b5a4a43f146
Anna Bagrowska <anna_bagrowska@wp.pl>
2018-10-08 11:12:32 +0200

    add nav

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+const nav = document.querySelector('nav')
 document.querySelector('.burger').addEventListener('click', function() {
-	this.classList.toggle('active')
-})
+	this.classList.toggle('active');
+	nav.classList.toggle('active');	
+}) 
+
+// const arrow = document.querySelector('.arrow');
+// const nav = document.querySelector('nav');
+
+// arrow.addEventListener('click', function() {
+// 	arrow.classList.toggle('on');
+// 	nav.classList.toggle('on');
+// })
