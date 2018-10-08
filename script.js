3a3a351f0ed6b9f6a2f47b7af88017c2e4835c70
Anna Bagrowska <anna_bagrowska@wp.pl>
2018-10-08 11:13:28 +0200

    remove comments

--- script.js
+++ script.js
@@ -3,11 +3,3 @@ document.querySelector('.burger').addEventListener('click', function() {
 	this.classList.toggle('active');
 	nav.classList.toggle('active');	
 }) 
-
-// const arrow = document.querySelector('.arrow');
-// const nav = document.querySelector('nav');
-
-// arrow.addEventListener('click', function() {
-// 	arrow.classList.toggle('on');
-// 	nav.classList.toggle('on');
-// })
